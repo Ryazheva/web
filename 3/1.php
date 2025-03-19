a30e4c3b90319090eefae9c06b66d635bfbf2ccd
Ryazheva <145559008+Ryazheva@users.noreply.github.com>
2025-03-19 23:11:17 +0300

    Update 1.php

--- 3/1.php
+++ 3/1.php
@@ -5,5 +5,4 @@ $very_bad_unclear_name = "15 chicken wings";
 $order = &$very_bad_unclear_name;
 $order .= ' and coca-cola';
 
-// Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.\n";
