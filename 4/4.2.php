8eaa6abf067dcbb50dee85861615da927d846794
Ryazheva <145559008+Ryazheva@users.noreply.github.com>
2025-04-04 17:38:35 +0300

    Update 4.2.php

--- 4/4.2.php
+++ 4/4.2.php
@@ -1,1 +1,28 @@
+<?php
+session_start();
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $_SESSION['teacher'] = [
+        'name' => htmlspecialchars($_POST['teacher_name']),
+        'subject' => htmlspecialchars($_POST['subject_type']),
+        'experience' => (int)$_POST['experience_count']
+    ];
+    header('Location: 4.1.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>4.4</title>
+    </head>
+    <body>
+        <form method="post">
+            <p>Введите имя преподавателя: <input type="text" name="name" /></p>
+            <p>Введите предмет: <input type="text" name="subject" /></p>
+            <p>Введите стаж: <input type="text" name="experience" /></p>
+            <input type="submit" value="Отправить">
+        </form>
+    </body>
+</html>
