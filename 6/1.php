d8c5eaea54d7b800d7c221b6dd1741f90f2c68e2
Ryazheva <145559008+Ryazheva@users.noreply.github.com>
2025-05-19 15:59:02 +0300

    Update 1.php

--- 6/1.php
+++ 6/1.php
@@ -8,29 +8,42 @@ echo "GET: " . $response . "\n";
 
 // 2. POST запрос
 $ch = curl_init('https://jsonplaceholder.typicode.com/posts');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_POST, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['title'=>'Hi','body'=>'Test','userId'=>1]));
+curl_setopt_array($ch, [
+    CURLOPT_POST => true,
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+    CURLOPT_POSTFIELDS => json_encode([
+        'title' => 'New post',
+        'body' => 'Content',
+        'userId' => 1
+    ])
+]);
 $response = curl_exec($ch);
 curl_close($ch);
 echo "POST: " . $response . "\n";
 
 // 3. PUT запрос
 $ch = curl_init('https://jsonplaceholder.typicode.com/posts/1');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
-curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['id'=>1,'title'=>'New','body'=>'Edit','userId'=>1]));
+curl_setopt_array($ch, [
+    CURLOPT_CUSTOMREQUEST => 'PUT',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_HTTPHEADER => ['Content-Type: application/json'],
+    CURLOPT_POSTFIELDS => json_encode([
+        'title' => 'Updated title',
+        'body' => 'Updated content',
+        'userId' => 1
+    ])
+]);
 $response = curl_exec($ch);
 curl_close($ch);
 echo "PUT: " . $response . "\n";
 
 // 4. DELETE запрос
 $ch = curl_init('https://jsonplaceholder.typicode.com/posts/1');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
+curl_setopt_array($ch, [
+    CURLOPT_CUSTOMREQUEST => 'DELETE',
+    CURLOPT_RETURNTRANSFER => true
+]);
 $response = curl_exec($ch);
 curl_close($ch);
 echo "DELETE: " . $response . "\n";
-?>
